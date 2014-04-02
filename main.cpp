f0dd4d122a001f19cf2267a2337c40164f3a02f8
Ellis Kenyo <elken.tdos@gmail.com>
2014-04-02 15:36:50 +0000

    UI is now read by main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
  * main.cc
  * Copyright (C) 2014 elken <elken.tdos@gmail.com>
@@ -16,12 +15,81 @@
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include <gtkmm-3.0/gtkmm.h>
 #include <iostream>
+#define UI_FILE "gsteg.ui"
 
 Gtk::Image* image = 0;
 Gtk::Box* box = Gtk::manage(new Gtk::Box());
+/*
+int encode(const char* iBmp, const char* oBmp, const char* txt)
+{
+    std::ifstream i(iBmp, std::ios::binary | std::ios::ate), t(txt, std::ios::ate);
+    std::ofstream o(oBmp, std::ios::binary);
+
+    if(o.bad() == true)
+    {
+        o << " " << std::endl;
+        o.close();
+    }
+    
+    if(i.is_open() && o.is_open() && t.is_open())
+    {
+        const std::streampos iSize = i.tellg();
+        const std::streampos tSize = t.tellg();
+        char* header = new char [54];
+        char* eBuf = new char [sizeof(int(iSize)-54)];
+        char* tBuf = new char [tSize];
+
+        i.seekg(0, std::ios::beg);
+        t.seekg(0, std::ios::beg);
+        i.read(header, 54);
+        i.seekg(54);
+        i.read(eBuf, int(iSize)-54);
+        t.read(tBuf, tSize);
+        o.write(header, 54);
+        o << char(3);
+        o.write(tBuf, tSize);
+        o << char(7);
+        o.write(eBuf, sizeof(eBuf));
+        return 0;
+    }
+    else
+    {
+        std::cerr << "I am error." << std::endl;
+        return 1;
+    }
+}
+
+int decode(const char* iBmp)
+{
+    std::ifstream i(iBmp, std::ios::binary | std::ios::ate);
+
+    if(i.is_open())
+    {
+        std::streampos iSize = i.tellg();
+        i.seekg(55);
+        char* tBuf = new char[int(iSize)-54];
+
+        for(int j=0;i.peek()!=char(7);j++)
+        {
+            tBuf[j] = i.get();
+        }
+
+        for(int k=0;k<(int(iSize)-54);k++)
+        {
+            std::cout << tBuf[k];
+        }
+
+        return 0;
+    }
+    else
+    {
+        std::cerr << "I am error." << std::endl;
+        return 1;
+    }
+}
+*/
 static void on_open_image()
 {
 	Gtk::FileChooserDialog dialog("Open image", Gtk::FILE_CHOOSER_ACTION_OPEN);
@@ -55,22 +123,33 @@ int main (int argc, char *argv[])
 {
 	Gtk::Main kit(argc, argv);
         
-	Gtk::Window main_win;
-	main_win.set_title ("QSteg");
+	Gtk::Window* main_win;
+	main_win->set_title ("GSteg");
 
+        Glib::RefPtr<Gtk::Builder> builder;
+        try
+        {
+            builder = Gtk::Builder::create_from_file(UI_FILE);
+        }
+        catch (const Glib::FileError & ex)
+        {
+            std::cerr << ex.what() << std::endl;
+            return 1;
+        }
+
+        builder->get_widget("gsteg_window", main_win);
 	box->set_orientation (Gtk::ORIENTATION_VERTICAL);
 	box->set_spacing (6);
-	main_win.add(*box);
+	main_win->add(*box);
 
 	image = Gtk::manage (new Gtk::Image ());
 	box->pack_start(*image, true, true);
 
 	Gtk::Button* button = Gtk::manage (new Gtk::Button("Open image..."));
-	button->signal_clicked ().connect(
-	                                  sigc::ptr_fun(&on_open_image));
+	button->signal_clicked ().connect( sigc::ptr_fun(&on_open_image));
 	box->pack_start(*button, false, false);
 
-	main_win.show_all_children();
-	kit.run(main_win);
+	main_win->show_all_children();
+	kit.run(*main_win);
 	return 0;
 }
